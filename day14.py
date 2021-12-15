1f797c05eef7c9dd88281ef0f284df3fb31a1ae6
Daniel Farushev <farushed@tcd.ie>
2021-12-15 20:49:03 +0000

    Cleaned up a little

--- day14.py
+++ day14.py
@@ -29,74 +29,42 @@ def part1():
     print(max(counter.values()) - min(counter.values()))
               
 def part2():
-    # start, rules = read()
-    
-    # start = list(start)
-    # for idk in range(10):
-    #     mask = [True for c in start]
-    #     insertions = []
-    #     for pair, to_insert in rules:
-    #         i = 0
-    #         while i < len(start)-1:
-    #             if start[i] == pair[0] and start[i+1] == pair[1]:
-    #                 new_index = i+1
-    #                 for insertion in insertions:
-    #                     if insertion[0] < new_index:
-    #                         new_index += 1
-    #                 for insertion in insertions:
-    #                     if insertion[0] > new_index:
-    #                         insertion[0] += 1
-    #                 insertions.append([new_index, to_insert])
-    #             # if mask[i] and mask[i+1]:
-    #             #     if start[i] == pair[0] and start[i+1] == pair[1]:
-    #             #         start.insert(i+1, to_insert)
-    #             #         mask.insert(i+1, False)
-    #                 # print(start, i, pair)
-    #             i += 1
-    #     # print(start)
-    #     # print(insertions)
-    #     for insertion in sorted(insertions, key=lambda x:x[0]):
-    #         start.insert(insertion[0], insertion[1])
-    # # print(start)
-    # counter = Counter(start)
-    # print(max(counter.values()) - min(counter.values()))  
-
     start, rules = read()
-    chain = defaultdict(lambda: 0)
+    first_letter, last_letter = start[0], start[-1]
+
+    zero_pairs = {rule[0]: 0 for rule in rules}
+    pairs = zero_pairs.copy()
     for i in range(len(start)-1):
-        chain[start[i:i+2]] += 1
+        pairs[start[i:i+2]] += 1
 
-    rules_d = dict()
+    new_pair_rules = zero_pairs.copy()
     for rule in rules:
-        rules_d[rule[0]] = str(rule[0][0] + rule[1]), str(rule[1] + rule[0][1])
-    # print(rules_d)
-    # print(chain)
+        new_pair_rules[rule[0]] = rule[0][0]+rule[1], rule[1]+rule[0][1]
 
+    for idk in range(100000):
+        new_pairs = zero_pairs.copy()
+        for rule in new_pair_rules:
+            num = pairs[rule]
+            if num > 0:
+                new_pairs[new_pair_rules[rule][0]] += num
+                new_pairs[new_pair_rules[rule][1]] += num
+        pairs = new_pairs
 
-    for idk in range(80):
-        new_chain = defaultdict(lambda: 0)
-        for rule in rules_d:
-            if chain[rule] > 0:
-                num = chain[rule]
-                # chain[rule] = 0
-                new_chain[rules_d[rule][0]] += num
-                new_chain[rules_d[rule][1]] += num
-        chain = new_chain
-
-    # print(sum(new_chain.values()))
     freqs = defaultdict(lambda: 0)
-    freqs[start[0]] = -1
-    freqs[start[-1]] = -1
-    for el, freq in chain.items():
+    freqs[first_letter], freqs[last_letter] = -1, -1
+    for el, freq in pairs.items():
         freqs[el[0]] += freq
         freqs[el[1]] += freq
-    freqs = {a: b//2 + (1 if a in [start[0], start[-1]] else 0) for a, b in freqs.items()}
+    freqs = {a: b//2 for a, b in freqs.items()}
+    freqs[first_letter] += 1
+    freqs[last_letter] += 1
+
     print(max(freqs.values()) - min(freqs.values()))
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    # part1()
+    # part1() # leaving the brute force here, but doing part2() with a range(10) in the loop will be *much* faster
     part2()
     end = time.perf_counter()
     print(f"{end-start} seconds taken")
